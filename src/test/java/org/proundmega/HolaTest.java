b9de52b7b038bde15aab9d907213f8b91ea260dd
vansi olivares <proundmega@hotmail.com>
2017-12-17 10:18:30 -0600

    Prueba añadida para compilar con Jenkins

---
+++ src/test/java/org/proundmega/HolaTest.java
@@ -0,0 +1,20 @@
+
+package org.proundmega;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author vansi
+ */
+public class HolaTest {
+    
+    public HolaTest() {
+    }
+    
+    @Test
+    public void hola() {
+        
+    }
+}
